37206d865a7942751c2d388336a167e8bc777cca
João Lucas <luckas2k20@gmail.com>
2022-01-25 00:00:35 -0300

    $ git commit -m

--- src/main/java/com/example/curso/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.curso.services.exceptions.DatabaseException;
 import com.example.curso.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Data base error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
---
+++ src/main/java/com/example/curso/services/exceptions/DatabaseException.java
@@ -0,0 +1,13 @@
+package com.example.curso.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super (msg);
+	}
+
+
+}
